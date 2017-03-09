f49ad52a136965f3b884d2b2920dda93bfb65861
mark <mark@debianstable.lan>
2017-03-09 22:23:46 +0300

    Initial commit

---
+++ build/deps/task2.d
@@ -0,0 +1,5 @@
+build/obj/task2.o build/deps/task2.d: src/task2.cpp include/classifier.h \
+ bridge/include/linear.h bridge/include/EasyBMP.h \
+ bridge/include/EasyBMP_DataStructures.h bridge/include/EasyBMP_BMP.h \
+ bridge/include/EasyBMP_VariousBMPutilities.h bridge/include/argvparser.h \
+ include/matrix.h include/matrix.hpp
